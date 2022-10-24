d11cdb5f3deabdecc8a20433e8e8006c8597d47f
JordiMateoUdL <jordi.mateo@udl.cat>
2022-10-24 05:08:56 -0500

    act03

---
+++ activitats/act03/helpers.h
@@ -0,0 +1,7 @@
+#ifndef _HELPERS_H_
+#define _HELPERS_H_
+
+int split_line(char *line, char* separator, char ***tokens);
+int read_line(char** line);
+
+#endif
---
+++ activitats/act03/main.c
@@ -0,0 +1,8 @@
+
+int
+main(int argc, char *argv[]){
+    // while not comand exit
+        // readline
+        // parseline
+        // launch comand
+}
